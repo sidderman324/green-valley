1405b10cefa8bd9f10fa7823d39abebd3a9b84f0
sidderman324 <s.i.sidorovkrd@yandex.ru>
2017-02-10 13:30:08 +0300

    Доделал single-room.html для трех расширений, сделал табы на главной, меню.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-  // весь ваш код
+
+  $('#toggler').on('click',function(){
+    $('#nav').toggle(400);
+  });
+
+// Табы промо
+  $('.type-tab__item').on('click', function(event){
+    event.preventDefault();
+    $('.type-tab__item a').toggleClass('type-tab__link--active');
+    $('.rest-type-head').toggleClass('rest-type-head--hidden');
+    $('.rest-option__item').toggleClass('rest-option__item--hidden');
+  });
+
+// Показать все отзывы
+  $('#reviewAll').on('click', function(event){
+    event.preventDefault();
+    $('.review-card').fadeIn(400).css('display', 'flex');
+  });
 });
